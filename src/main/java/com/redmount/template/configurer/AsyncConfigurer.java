66e5de6cda207163f5ab9707fbdfc1ad94f4e784
redmount <redmount@163.com>
2019-04-01 21:41:19 +0800

    去掉多余的启用异步注解. 都标识在主启动类里了.

--- src/main/java/com/redmount/template/configurer/AsyncConfigurer.java
+++ src/main/java/com/redmount/template/configurer/AsyncConfigurer.java
@@ -2,13 +2,11 @@ package com.redmount.template.configurer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
-@EnableAsync
 public class AsyncConfigurer {
     private int corePoolSize = 10;
     private int maxPoolSize = 200;
