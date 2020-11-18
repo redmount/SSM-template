e3e85116203213e3fd12fc2bacefa6bfd0551da3
朱峰 <dbzhufeng@travelsky.com>
2020-11-18 12:48:10 +0800

    增加swagger的开关, 在dev模式下才可用.

--- src/main/java/com/redmount/template/configurer/Swagger2Configurer.java
+++ src/main/java/com/redmount/template/configurer/Swagger2Configurer.java
@@ -1,6 +1,7 @@
 package com.redmount.template.configurer;
 
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.RequestHandler;
@@ -19,27 +20,40 @@ import java.util.function.Predicate;
 
 /**
  * Swagger2的配置类
+ *
  * @author 朱峰
  * @date 2018年11月12日
  */
 @Configuration
 @EnableSwagger2
 public class Swagger2Configurer {
+    /**
+     * 当前的运行模式
+     */
+    @Value("${spring.profiles.active}")
+    private String env;
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .enable("dev".equals(env))
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 .paths(PathSelectors.any())
                 .build();
     }
+
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Springboot利用Swagger构建API文档")
-                .description("简单优雅的RESTFul风格，http://github.com/redmount56/SSM-Template")
-                .termsOfServiceUrl("http://github.com/redmount56/SSM-Template")
-                .version("1.0")
-                .build();
+        if ("dev".equals(env)) {
+            return new ApiInfoBuilder()
+                    .title("Springboot利用Swagger构建API文档")
+                    .description("简单优雅的RESTFul风格，http://github.com/redmount56/SSM-Template")
+                    .termsOfServiceUrl("http://github.com/redmount56/SSM-Template")
+                    .version("1.0")
+                    .build();
+        } else {
+            return null;
+        }
     }
 }
